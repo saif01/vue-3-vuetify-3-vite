efbaa354da09dd7d08ec5f99aa416adaa7f3048f
Md. Syful Islam <syful.cse.bd@gmail.com>
2023-12-30 10:18:54 +0000

    Up from sandbox

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,11 +10,16 @@ export default {
 </script>
 
 <template>
-  <h1>Vue 3 + Vuetify + MDI </h1>
+  <h1>Vue 3 + Vuetify + Vite Application</h1>
   {{ test }}
   <h3>{{ counter }}</h3>
 
-  <v-btn color="success" class="mr-2" @click="counter++" icon="mdi-plus-circle"></v-btn>
+  <v-btn
+    color="success"
+    class="mr-2"
+    @click="counter++"
+    icon="mdi-plus-circle"
+  ></v-btn>
   <v-btn color="error" @click="counter--" icon="mdi-minus-circle"></v-btn>
 </template>
 
